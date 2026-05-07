c3a8621bb263f7a325cd212440f070bd56ce84b0
Richard <265088646+richardtavaszi-dev@users.noreply.github.com>
2026-05-07 13:07:51 +0200

    display

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from '@angular/core';
+import { GameItem } from './game-item';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,19 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
-  check(){}
+  tip: string = ""
+  gi: GameItem = new GameItem(1, "")
+  guesses: GameItem[] = []
+  generatedNumber: number = Math.floor(Math.random() * 10);
+
+  check(){
+    let tipNumber = parseInt(this.tip)
+    if(this.generatedNumber == tipNumber){ 
+      this.guesses.push(new GameItem(tipNumber, "Sikerült!"))
+    }
+    else if(this.generatedNumber > tipNumber){
+      this.guesses.push(new GameItem(tipNumber, "Nagyobbra gondoltam!"))
+    }
+    else{this.guesses.push(new GameItem(tipNumber, "Kisebbre gondoltam!"))}
+  }
 }
