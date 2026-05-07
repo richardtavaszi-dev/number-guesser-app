5f301bb03687916b29e7034951d855862832fadc
Richard <265088646+richardtavaszi-dev@users.noreply.github.com>
2026-05-07 12:51:44 +0200

    constructor added

--- src/app/game-item.ts
+++ src/app/game-item.ts
@@ -1,1 +1,8 @@
-export class GameItem {}
+export class GameItem {
+    guess: number = 0
+    response: string = ""
+    constructor(guess: number, response: string){
+        this.guess = guess
+        this.response = response
+    }
+}
